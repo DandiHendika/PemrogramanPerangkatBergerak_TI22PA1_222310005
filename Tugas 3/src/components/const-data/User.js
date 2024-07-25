4e539a4c08a1613816a0dccea2bdc70bbb98856d
DandiHendika <113397399+DandiHendika@users.noreply.github.com>
2024-07-25 22:19:30 +0700

    Add files via upload

---
+++ Tugas 3/src/components/const-data/User.js
@@ -0,0 +1,9 @@
+const Users = [
+    { id: 1, name: "Dandi", fullname: "Dandi Hendika", gender: "M" },
+    { id: 2, name: "Budi", fullname: "Budi Santoso", gender: "M" },
+    { id: 3, name: "Cici", fullname: "Cici Hartono", gender: "F" },
+    { id: 4, name: "Dedi", fullname: "Dedi Suharto", gender: "M" },
+    { id: 5, name: "Erik", fullname: "Erik Saputra", gender: "M" }
+];
+
+export { Users };
